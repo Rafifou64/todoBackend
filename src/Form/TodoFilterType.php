a0bb50fe2a1c8d8aa49d6008f1f5062ae81b640e
Rafifou64 <126586065+Rafifou64@users.noreply.github.com>
2023-05-31 11:58:21 +0200

    Form pour tri des tâches non finies

--- src/Form/TodoFilterType.php
+++ src/Form/TodoFilterType.php
@@ -4,18 +4,16 @@ namespace App\Form;
 
 use App\Entity\Todo;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TodoType extends AbstractType
+class TodoFilterType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('done')
-        ;
+            ->add('stillTodo', CheckboxType::class, ['mapped'=>false, 'label'=>'Afficher les tâches non effectuées', 'required'=>false]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
